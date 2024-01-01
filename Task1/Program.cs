027f3317cd822ce7b58ca26a4a70f20ec94b6650
Elena Lyzo <elena.lyzo.69@gmail.com>
2024-01-01 23:16:54 +0300

    Добавили файл

---
+++ Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Проверка кратности числа 7 и 23
+
+int a = 50;
+int b = 7;
+int c = 322;
+
+Console.WriteLine(" ");
+
+if (a % 7 == 0 && a % 23 == 0)
+{
+    Console.WriteLine("Да");
+    Console.WriteLine(" ");
+}
+else
+{
+    Console.WriteLine("Нет");
+    Console.WriteLine("  ");
+    
+}
+
+
+if (b % 7 == 0 && b % 23 == 0)
+{
+    Console.WriteLine("Да");
+    Console.WriteLine("  ");
+}
+else
+{
+    Console.WriteLine("Нет");
+    Console.WriteLine("  ");
+}
+
+if (c % 7 == 0 && c % 23 == 0)
+{
+    Console.WriteLine("Да");
+    Console.WriteLine("  ");
+}
+else
+{
+    Console.WriteLine("Нет");
+    Console.WriteLine("  ");
+}
