4ba37a8ea75e3d0b3b8ddba4175b14b64971ffc5
Elena Lyzo <elena.lyzo.69@gmail.com>
2024-01-02 18:16:18 +0300

    задача 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Показ наибольшей цифры числа
+
+Console.Write("Введите число из отрезка [10, 99]: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+
+Console.WriteLine(maxDigit);
